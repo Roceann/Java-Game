28e51f6546cddc865045571a87c92ac0cca49394
abdelkader <abdelkader.lakhdar@epitech.eu>
2025-11-06 11:47:28 +0100

    HUD 90% pret + Classe Entity

--- Game/core/src/main/java/io/github/dr4c0nix/survivorgame/Main.java
+++ Game/core/src/main/java/io/github/dr4c0nix/survivorgame/Main.java
@@ -1,11 +1,12 @@
 package io.github.dr4c0nix.survivorgame;
 
 import com.badlogic.gdx.Game;
+import io.github.dr4c0nix.survivorgame.entities.Entity;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
     @Override
     public void create() {
-        setScreen(new FirstScreen());
+        setScreen(new Menu());
     }
 }
