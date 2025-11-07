c25f22fc976ba77a1fece2e6edb616c0187533d5
Alexandre Drain <alexandre.drain@epitech.eu>
2025-11-07 17:17:28 +0100

    add map v1

---
+++ Game/assets/Map/items1.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="items1" tilewidth="32" tileheight="32" tilecount="3072" columns="64">
+ <image source="img/DungeonCrawl_ProjectUtumnoTileset.png" width="2048" height="1536"/>
+ <tile id="1209">
+  <animation>
+   <frame tileid="1209" duration="150"/>
+   <frame tileid="1210" duration="150"/>
+   <frame tileid="1211" duration="150"/>
+   <frame tileid="1212" duration="150"/>
+  </animation>
+ </tile>
+</tileset>
