078440bf40e5068710f5dd3283abb85519266416
Alexandre Drain <alexandre.drain@epitech.eu>
2025-11-19 23:51:23 +0100

    [ADD] base bow

--- Game/core/src/main/java/io/github/dr4c0nix/survivorgame/weapon/Bow.java
+++ Game/core/src/main/java/io/github/dr4c0nix/survivorgame/weapon/Bow.java
@@ -6,25 +6,29 @@ import io.github.dr4c0nix.survivorgame.entities.player.Player;
 public class Bow extends Weapon {
 
     public Bow(EntityFactory factory) {
-        super(factory);
-        this.damage = 1.5f;                         
-        this.cadence = 0.8f;                       
-        this.range = 10;                             
-        this.critChance = 0.15f;                    
-        this.critDamage = 2.2f;                     
-        this.projectileSpeed = 3.5f;                
-        this.projectileCount = 1.0f;                
-        this.duration = 1.0f;                       
-        this.pierce = 1;      
-        //this.EffectType = EffectType.NONE;;                      
-        this.description = "Un arc romeesque permettant de grequer.";
-        this.iconPath = "weapon/bow.png";           
+        super(factory,
+            1.5f,
+            0.8f,
+            10,
+            0.15f,
+            3.5f,
+            1,
+            1.0f,
+            1,
+            24f,
+            6f,
+            "Weapon/Bow/arrow.png",
+            "Un arc romeesque permettant de grequer.",
+            "Weapon/bow.png");
     }
 
     @Override
     public void update(float delta, Player player) {
-        System.out.println("TEST");
+        cooldownTick(delta);
+        if (canShoot()) {
+            // entityFactory pour obtenir un projectile et spawn // plus tard
+            resetCooldown();
+            System.out.println("Bow fired: damage=" + damage);
+        }
     }
-
-    
 }
