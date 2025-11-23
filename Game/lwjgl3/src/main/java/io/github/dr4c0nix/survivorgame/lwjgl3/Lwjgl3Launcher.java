12c9c3e03df97d3ba86ee8aeb260add8cd6f7c9f
Roceann <raphael.malet2005@gmail.com>
2025-11-23 23:11:57 +0100

    [new] GDD + naming correction

--- Game/lwjgl3/src/main/java/io/github/dr4c0nix/survivorgame/lwjgl3/Lwjgl3Launcher.java
+++ Game/lwjgl3/src/main/java/io/github/dr4c0nix/survivorgame/lwjgl3/Lwjgl3Launcher.java
@@ -79,7 +79,11 @@ public class Lwjgl3Launcher {
         //// This uses the dependency `com.badlogicgames.gdx:gdx-lwjgl3-angle` to function.
         //// You can choose to remove the following line and the mentioned dependency if you want; they
         //// are not intended for games that use GL30 (which is compatibility with OpenGL ES 3.0).
-        configuration.setOpenGLEmulation(Lwjgl3ApplicationConfiguration.GLEmulation.ANGLE_GLES20, 0, 0);
+        // Only enable ANGLE emulation on Windows (avoid EGL errors on Linux / WSL)
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")) {
+            configuration.setOpenGLEmulation(Lwjgl3ApplicationConfiguration.GLEmulation.ANGLE_GLES20, 0, 0);
+        }
 
         return configuration;
     }
